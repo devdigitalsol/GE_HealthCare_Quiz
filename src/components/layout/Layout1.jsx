c56507b98c0ad556272b8e8e6739704366d138cd
devdigitalsol <digital@solmc.in>
2023-09-04 16:21:37 +0530

    first commit

---
+++ src/components/layout/Layout1.jsx
@@ -0,0 +1,20 @@
+import { Outlet } from "react-router-dom";
+import MAINLOGO from "./../../images/main_logo.png";
+import BOTTOMLOGO from "./../../images/bottom_logo.png";
+import "../../utils/app.css";
+
+export default function Layout1() {
+  return (
+    <div className="max-w-md mx-auto min-h-full layout1 flex flex-col items-center justify-around">
+      <div className="mx-auto w-36 p-4">
+        <img src={MAINLOGO} alt="logo" />
+      </div>
+      <Outlet />
+      <div className="mx-auto w-full">
+        <img src={BOTTOMLOGO} alt="logo" />
+      </div>
+      {/* <p className=" absolute bottom-0 text-xs font-thin ">JB00326BS</p> */}
+      <p className=" absolute bottom-0  text-small">JB00326BS</p>
+    </div>
+  );
+}
